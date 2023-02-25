331d57bce6e84250a578e16fc1add571b0773eb5
nlywto5b <118055561+nlywto5b@users.noreply.github.com>
2023-02-25 15:25:10 +0800

    1.5.5

--- launcher.py
+++ launcher.py
@@ -9,7 +9,7 @@ except ImportError:  # 检测库/文件是否成功调用, 否则显示提示信
 请确认Tcl/TK是否在你的电脑上安装! ''')
 else:
 
-    def run_game():
+    def run():
         '''运行游戏'''
         global width, height
         # 获取游戏窗口大小
@@ -100,7 +100,7 @@ else:
                                 width=20,
                                 height=2,
                                 font=('Minecraft AE Pixel', 22),
-                                command=run_game)
+                                command=run)
     start_button.place(x=300, y=350, anchor='s')
     # 主循环
     window.mainloop()
